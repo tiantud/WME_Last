dcbab91fef0d54b6a33614a2d2e67cbd1d0e98cd
Zhao Yanchen <banjobelphegor@gmail.com>
2018-01-21 18:41:47 +0100

    changed the source of map

--- Team_6/public/script.js
+++ Team_6/public/script.js
@@ -236,15 +236,12 @@ function set_table(table_id, data, selected_property){
  ********************************************************/
 
  function initMap(){
-   var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	
+	var mymap = L.map('mapid').setView([51.505, -0.09], 2);
 
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.streets'
-	}).addTo(mymap);
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
 
 	L.marker([51.5, -0.09]).addTo(mymap)
 		.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
